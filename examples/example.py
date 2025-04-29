d04eb6e15a091014e33a588b31ab4ee45b1abd58
Jack <jack74387@gmail.com>
2025-04-30 00:37:00 +0800

    can hide more bits

--- examples/example.py
+++ examples/example.py
@@ -2,8 +2,7 @@ from stegano.encoder import StegoEncoder
 from stegano.decoder import StegoDecoder
 
 if __name__ == '__main__':
-    # 範例：將 "HELLO" 藏入 examples/input.png
-    message = b"HELLO"
+    message = b"hello this is a test message" * 10
     encoder = StegoEncoder(seed=123)
     encoder.encode_image("examples/4.1.01.tiff", "output/test.tiff", message)
 
--- stegano/decoder.py
+++ stegano/decoder.py
@@ -4,9 +4,6 @@ from .knight import KnightTour
 from .utils import get_lsb, get_lsb2
 
 class StegoDecoder:
-    """
-    依照 Encode 規則反向讀取並 RS 解碼
-    """
     def __init__(self, seed: int = 0):
         self.seed = seed
         self.rs = RSCoder()
@@ -14,41 +11,45 @@ class StegoDecoder:
     def decode_image(self, stego_path: str) -> bytes:
         img = Image.open(stego_path).convert('RGB')
         pixels = img.load()
-        w,h = img.size
+        w, h = img.size
         knight = KnightTour(w, h, seed=self.seed)
 
-        start = (0,0)
+        used = set()
+        start = (0, 0)
         result = bytearray()
+
         while True:
-            path = knight.generate_path(start[0], start[1], 12)
-            # 讀 data bits
+            path = knight.generate_path(start[0], start[1], 12, used=used)
+            used.update(path)
+
+
+            # 提取前 11 pixels 的 data bits (3 bits/pixel)
             data_bits = ''
-            for x,y in path[:11]:
-                r,g,b = pixels[x,y]
+            for (x, y) in path[:11]:
+                r, g, b = pixels[x, y]
                 data_bits += str(get_lsb(r)) + str(get_lsb(g)) + str(get_lsb(b))
-            # 32 bits data + 1bit end
             block_bits = data_bits[:32]
             end_flag = data_bits[32]
-            data_bytes = bytes(int(block_bits[i:i+8],2) for i in range(0,32,8))
-            # 讀 parity bits
-            parity_bits = ''
-            for x,y in path[:8]:
-                r,g,b = pixels[x,y]
-                parity_bits += str(get_lsb2(r)) + str(get_lsb2(g)) + str(get_lsb2(b))
-            parity_bytes = bytes(int(parity_bits[i:i+8],2) for i in range(0,24,8))
+            data_bytes = bytes(int(block_bits[i:i+8], 2) for i in range(0, 32, 8))
 
-            # RS 解碼
+            # 提取前 8 pixels 的 parity bits (3 bits/pixel)
+            parity_bits = ''
+            for (x, y) in path[:8]:
+                r, g, b = pixels[x, y]
+                parity_bits += str(get_lsb2(r)) + str(get_lsb2(g)) + str(get_lsb2(b))
+            parity_bytes = bytes(int(parity_bits[i:i+8], 2) for i in range(0, 24, 8))
+
             encoded = data_bytes + parity_bytes
             try:
                 decoded = self.rs.decode(encoded)
-            except Exception as e:
-                print("Too many errors, stop decoding.")
+                result.extend(decoded)
+            except Exception:
+                print("Too many errors to correct. Decoding stopped.")
                 break
-            result.extend(decoded)
 
             if end_flag == '1':
                 break
             start = path[-1]
 
-        # 去掉 padding zero
+        # 移除 padding
         return bytes(result).rstrip(b'\x00')
